4edcbceb9a9e825fe93a47bd42bc46d4c87cdf3e
Muduasir Bashir <for.all6474@gmail.com>
2024-05-07 20:23:15 +0500

    Prompt box component designed

--- frontend/src/app/(Pages)/MarketPlace/page.js
+++ frontend/src/app/(Pages)/MarketPlace/page.js
@@ -1,10 +1,13 @@
 import Filter from '@/app/Components/Filter/Filter';
 import styles from '@/app/(Pages)/Marketplace/Marketplace.module.css'
+import PromptBox from '@/app/Components/(liteComponents)/PromptBox/PromptBox';
 const MarketPlace = () => {
   return (
     <div className={styles.mainContainer}>
       <Filter />
-      <div className={styles.leftContainer}>sfdas</div>
+      <div className={styles.leftContainer}>
+        <PromptBox />
+      </div>
     </div>
   )
 }
