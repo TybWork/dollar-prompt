eab581dc745d277da665c70e02ee4eee39bd8e69
Muduasir Bashir <for.all6474@gmail.com>
2024-05-07 22:57:00 +0500

    purchase credit component designed

--- frontend/src/app/(Pages)/MarketPlace/page.js
+++ frontend/src/app/(Pages)/MarketPlace/page.js
@@ -1,12 +1,16 @@
 import Filter from '@/app/Components/Filter/Filter';
 import styles from '@/app/(Pages)/Marketplace/Marketplace.module.css'
 import PromptBox from '@/app/Components/(liteComponents)/PromptBox/PromptBox';
+import PurchaseCredits from '@/app/Components/PurchaseCredits/PurchaseCredits';
 const MarketPlace = () => {
   return (
     <div className={styles.mainContainer}>
       <Filter />
       <div className={styles.leftContainer}>
-        <PromptBox />
+        {/* <PromptBox /> */}
+
+        {/* purchase credits */}
+        <PurchaseCredits />
       </div>
     </div>
   )
