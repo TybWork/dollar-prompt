72fd726242928f87d4ba9f6ac4ebab10610987a8
Muduasir Bashir <for.all6474@gmail.com>
2024-05-08 18:42:43 +0500

    our apps page margin added

--- frontend/src/app/(Pages)/our-apps/page.jsx
+++ frontend/src/app/(Pages)/our-apps/page.jsx
@@ -2,7 +2,7 @@ import styles from '@/app/(Pages)/our-apps/our-apps.module.css'
 import Slider from '@/app/Components/Slider/Slider'
 const page = () => {
     return (
-        <div>
+        <div className={styles.parentContainer}>
             <Slider />
             <Slider />
             <Slider />
@@ -10,4 +10,4 @@ const page = () => {
     )
 }
 
-export default page
+export default page;
