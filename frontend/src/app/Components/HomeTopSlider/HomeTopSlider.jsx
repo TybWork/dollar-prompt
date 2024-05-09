b2df330f9da184d6bde4368c6de13ce4b3357fe6
Muduasir Bashir <for.all6474@gmail.com>
2024-05-09 14:13:57 +0500

    home top slider set

--- frontend/src/app/Components/HomeTopSlider/HomeTopSlider.jsx
+++ frontend/src/app/Components/HomeTopSlider/HomeTopSlider.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import styles from "@/app/Components/HomeTopSlider/HomTopSlider.module.css"
 import GradientButton from '../GradientButton/GradientButton'
 import BorderButton from '../BorderButton/BorderButton'
+import Image from 'next/image'
 
 const HomeTopSlider = () => {
     return (
         <div className={styles.sliderContainer}>
-            <img src="/assets/imageAssets/Home/HomeTopSlider.webp" alt="demoHeader" />
+            {/* <img src="/assets/imageAssets/Home/HomeTopSlider.webp" alt="demoHeader" /> */}
+            <Image src="/assets/imageAssets/Home/HomeTopSlider.webp" alt='topSlider' width={0} height={0} sizes='100vw' className={styles.sliderImage} />
             <div className={styles.content}>
                 <h1 className={styles.heading}>Prompt Marketplace</h1>
                 <h2 className={styles.subHeading}>Search 100,000 AI prompts from the world's best AI creators</h2>
