0d160bb533c13242f12f05c1bb5ba1fa21f8baa5
Muduasir Bashir <for.all6474@gmail.com>
2024-05-15 14:11:04 +0500

    itemTopList and animated heading component designed

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -7,6 +7,10 @@ import SimpleSlider from "./Components/SimpleSlider/SimpleSlider";
 import SinglePrompt from "./Components/SinglePrompt/SinglePrompt";
 import { useDispatch, useSelector } from "react-redux";
 import { increment } from "./Redux/Features/counter/CounterSlice";
+import Slider from "./Components/Slider/Slider";
+import AnimatedHeading from "./Components/(liteComponents)/AnimatedHeading/AnimatedHeading";
+import ItemTopList from "./Components/ItemTopList/ItemTopList";
+import TopItemSingleList from "./Components/TopItemSingleList/TopItemSingleList";
 export default function Home() {
   const dispatch = useDispatch();
   const count = useSelector((state) => state.counter.value)
@@ -15,16 +19,73 @@ export default function Home() {
       {/* top slider */}
       <HomeTopSlider />
       {/* FeatureCard card */}
-      <FeatureCard />
-      {/* Service`s Card */}
-      <SinglePromptCard />
-      {/* SimpleSlider */}
-      <SimpleSlider />
-      {/* single prompt */}
-      <SinglePrompt />
+      <Slider component={<FeatureCard />} />
 
-      <button onClick={() => dispatch(increment())}>increment</button>
-      <div>{count}</div>
+      {/* item top list */}
+      <ItemTopList mainComponent={<Slider component={<TopItemSingleList />} />} />
+
+      {/*Hire Card */}
+      <Slider component={<FeatureCard />} />
+
+      {/* featured card */}
+      <AnimatedHeading title="Featured Prompts" />
+      <Slider component={<SinglePromptCard />} />
+
+      {/* featured card */}
+      <AnimatedHeading title="Newest Prompts" />
+      <Slider component={<SinglePromptCard />} />
+
+
+      {/* top item single list */}
+      <div className={styles.threeCategoriesContainer}>
+        <ItemTopList mainComponent={<TopItemSingleList />} />
+        <ItemTopList mainComponent={<TopItemSingleList />} />
+        <ItemTopList mainComponent={<TopItemSingleList />} />
+      </div>
+
+      {/* most popular prompt bundles */}
+      <AnimatedHeading title="Most Popular Prompts Bundles" />
+      <Slider component={<SinglePromptCard />} />
+
+      {/* build an app slider */}
+      <SimpleSlider />
+
+      {/* Art & Illustration Prompts */}
+      <AnimatedHeading title="Art & Illustration Prompts" />
+      <Slider component={<SinglePromptCard />} />
+
+      {/* Logo & Icon Prompts */}
+      <AnimatedHeading title="Logo & Icon Prompts" />
+      <Slider component={<SinglePromptCard />} />
+
+      {/* Portrait Photography Prompts */}
+      <AnimatedHeading title="Portrait Photography Prompts" />
+      <Slider component={<SinglePromptCard />} />
+
+      {/* build an app slider */}
+      <SimpleSlider />
+
+      {/* Pattern Prompts */}
+      <AnimatedHeading title="Pattern Prompts" />
+      <Slider component={<SinglePromptCard />} />
+
+      {/* Sticker Prompts */}
+      <AnimatedHeading title="Sticker Prompts" />
+      <Slider component={<SinglePromptCard />} />
+
+      {/* T-shirt Print Prompts */}
+      <AnimatedHeading title="T-shirt Print Prompts" />
+      <Slider component={<SinglePromptCard />} />
+
+      {/* Poster Print Prompts*/}
+      <AnimatedHeading title="Poster Print Prompts" />
+      <Slider component={<SinglePromptCard />} />
+
+      {/* Explore the App Store slider */}
+      <SimpleSlider />
+
+      {/* <button onClick={() => dispatch(increment())}>increment</button>
+      <div>{count}</div> */}
     </div>
   );
 }
