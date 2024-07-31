fdbf9c87d3de776778aadbd5e79b3ba29f85e968
Muduasir Bashir <for.all6474@gmail.com>
2024-08-01 00:22:47 +0500

    sell page responsiveness done

--- frontend/src/app/(Pages)/Sell/firststep/First.js
+++ frontend/src/app/(Pages)/Sell/firststep/First.js
@@ -1,5 +1,6 @@
 import GradientButton from '@/app/Components/GradientButton/GradientButton'
-import styles from '@/app/Components//SellingProduct/SellingProduct.module.css'
+// import styles from '@/app/Components//SellingProduct/SellingProduct.module.css'
+import styles from '@/app/(Pages)/sell/firststep/First.module.css'
 const SellingProduct = ({ onNext }) => {
     return (
         <div className={styles.mainContainer}>
@@ -17,8 +18,8 @@ const SellingProduct = ({ onNext }) => {
             </div>
 
             {/* rightCol */}
-            <div className="rightCol">
-                <iframe width="600" height="337" src="https://www.youtube.com/embed/nnT9fXw2T4c" title="MGT211 Short Lecture 3_Highlighted Questions_MGT211short Lectures_Mid Term_Full Detail In Short Time" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <div className={styles.rightCol}>
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/nnT9fXw2T4c" title="MGT211 Short Lecture 3_Highlighted Questions_MGT211short Lectures_Mid Term_Full Detail In Short Time" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
             </div>
         </div >
     )
