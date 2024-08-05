19aaf1bcf01260b21a39ab9931598a3226e6d842
Muduasir Bashir <for.all6474@gmail.com>
2024-08-05 15:37:13 +0500

    nextjs.config changes for alias @

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,24 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // next.config.js
+//   images: {
+//     domains: ['res.cloudinary.com'],
+//   },
+
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // next.config.js
   images: {
     domains: ['res.cloudinary.com'],
   },
-
+  webpack: (config) => {
+    // Adding path alias configuration
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
